d741d0ab6bbd33fdb957e4ec091d046e4ef393ee
Kyle Baker <tokylebaker@gmail.com>
2019-09-12 17:04:41 -0700

    path finder algorithm

--- treasure-hunter/src/app/App.js
+++ treasure-hunter/src/app/App.js
@@ -1,44 +1,42 @@
 import React, { useState, useEffect } from "react";
 import logo from "../logo.svg";
 import "./App.css";
-import "../../node_modules/react-vis/dist/style.css"
+import "../../node_modules/react-vis/dist/style.css";
 import { ActionButtons } from "./ActionButtons";
-import Map from './Map/Map'
+import Map from "./Map/Map";
 import axios from "axios";
 import config from "config";
 import { init, getAllRooms, getPath } from "../db";
 import { default as withActions } from "./actions";
-import {Room} from "../app/room/room";
+import { Room } from "../app/room/room";
 
 const App = () => {
-  const [currentRoom, setCurrentRoom] = useState(null)
-  const [player, setPlayer] = useState(null)
-  const [rooms, setRooms] = useState([])
-  const [path, setPath] = useState([])
+  const [currentRoom, setCurrentRoom] = useState(null);
+  const [player, setPlayer] = useState(null);
+  const [rooms, setRooms] = useState([]);
+  const [path, setPath] = useState([]);
 
   useEffect(() => {
     axios.defaults.headers.common["Authorization"] = `Token ${config.API_KEY}`;
 
     init().then(res => {
-      console.log(res)
-      setPath([...res.path])
+      console.log(res);
+      setPath([...res.path]);
       setCurrentRoom(res.room);
     });
-
   }, []);
 
   useEffect(() => {
     getAllRooms()
-    .then(newRooms => {
-      return setRooms(newRooms)
-    })
-    .then(rooms => {
-      return getPath()
-    })
-    .then(path => setPath(path))
-    .catch(err => console.log(err))
-  }, [currentRoom])
-
+      .then(newRooms => {
+        return setRooms(newRooms);
+      })
+      .then(rooms => {
+        return getPath();
+      })
+      .then(path => setPath(path))
+      .catch(err => console.log(err));
+  }, [currentRoom]);
 
   return (
     <div className="App">
@@ -47,9 +45,9 @@ const App = () => {
         setCurrentRoom={setCurrentRoom}
       />
       <Map rooms={rooms} path={path} currentRoom={currentRoom} />
-      <Room currentRoom={currentRoom}></Room>
+      <Room currentRoom={currentRoom} />
     </div>
   );
-}
+};
 
 export default withActions(App);
